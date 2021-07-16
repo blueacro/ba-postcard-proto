19e0ba163f2c0efa9b0b2959e8e7011600e3f545
Yann Ramin <github@theatr.us>
2021-07-16 15:23:15 -0700

    Add debug and missing public

--- src/lib.rs
+++ src/lib.rs
@@ -2,27 +2,27 @@
 
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct TimeIsNow {
     pub seconds: u8,
     pub minutes: u8,
     pub hours: u8
 }
 
-#[derive(Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct SetTime {
-    seconds: u8,
-    minutes: u8,
-    hours: u8,
+    pub seconds: u8,
+    pub minutes: u8,
+    pub hours: u8,
 }
 
-#[derive(Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub enum Command {
     SetTime(SetTime),
     QueryTime,
 }
 
-#[derive(Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub enum Response {
     TimeIsNow(TimeIsNow)
 }
